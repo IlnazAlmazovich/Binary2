98d28b0a099c89d0ca93b5a6c4dc630acbf439e2
farhullin2017@gmail.com <90451035+IlnazFarkhullin@users.noreply.github.com>
2023-12-05 12:41:37 +0300

    binary

---
+++ Binary/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; // Подключаем просторанство имён для работы с файлами
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Binary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\Users\\Пользователь\\Desktop\\Binary\\test.txt"; //указываем путь к файлу
+
+            //Запись в файл
+            using (BinaryWriter bw = new BinaryWriter(new FileStream(path,FileMode.OpenOrCreate))) // создаём поток для записи
+           //Использование BinaryWriter с FileStream упрощает преобразование данных различных типов в бинарный формат,
+           //что позволяет сохранить эти данные в файле. Это особенно полезно, когда необходимо сохранить структурированные данные,
+           //которые могут быть легко восстановлены позднее.
+            {
+                // Данные для записи
+                string text = "Расцветали яблони и груши,\r\nПоплыли туманы над рекой.\r\nВыходила на берег Катюша,\r\nНа высокий берег на крутой.\r\nВыходила, песню заводила\r\nПро степного сизого орла,\r\nПро того, которого любила,\r\nПро того, чьи письма берегла.";
+                int group = 228;
+                double txt = 22.8;
+                //Записываем данные
+                bw.Write(text);
+                bw.Write(group);
+                bw.Write(txt);
+
+                Console.WriteLine("Данные которые записались: \n");
+                Console.WriteLine(text);
+                Console.WriteLine(group);
+                Console.WriteLine(txt);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+            //Чтение из файла
+            using (BinaryReader br = new BinaryReader(new FileStream(path, FileMode.OpenOrCreate, FileAccess.Read)))//Создаём поток для чтения
+            {
+                Console.WriteLine("Данные из файла: ");
+                Console.WriteLine(br.ReadString()); // считываем данные типа string
+                Console.WriteLine(br.ReadInt32()); // считываем данные типа int
+                Console.WriteLine(br.ReadDouble()); // считываем данные типа double
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BinaryArray/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; // Подключаем просторанство имён для работы с файлами
+using System.Runtime.Remoting.Messaging;
+
+namespace BinaryArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\Users\\Пользователь\\Desktop\\Binary\\test2.txt"; //указываем путь к файлу
+            // если файла test2.txt нет, то программа создаст его автоматически
+            // Если нет файла с которым мы работаем, то в любых потоках он создаства автоматически
+
+            // Создаём массив
+            int[] array = { 1, 2, 3 };
+
+            //Записываем массив в файл
+            using (BinaryWriter writer = new BinaryWriter(new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite)))
+            {
+                foreach (int num in array)
+                {
+                    writer.Write(num);
+                    Console.WriteLine(num);
+                }
+            }
+            Console.WriteLine("YES");
+
+
+
+            //Читаем массив
+            using (BinaryReader read = new BinaryReader(new FileStream(path, FileMode.Open, FileAccess.Read)))
+            {
+                //Создаём новый массив для прочитанных данных
+                int[] newAr = new int[array.Length];
+                
+
+                // перебераем и считываем массив
+                for (int i = 0; i < newAr.Length; i++)
+                {
+                    newAr[i] = read.ReadInt32();
+                }
+
+                //выводим массив на консоль
+                foreach (int num in newAr)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+            Console.ReadKey();
+            
+                                                                               
+        }
+    }
+}
